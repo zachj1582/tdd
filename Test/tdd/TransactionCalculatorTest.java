8da9e21b0741cb106d0f697e631ba014660db8cd
Zachary Johnson <zach_j@att.net>
2020-07-19 12:20:48 -0500

    case 3 of add taxes accounting for imported and exempt passes

--- Test/tdd/TransactionCalculatorTest.java
+++ Test/tdd/TransactionCalculatorTest.java
@@ -50,8 +50,9 @@ public class TransactionCalculatorTest {
     public void test_addTaxesAccountingForImportedAndExempt(){
         cashRegister.addProductToTransaction("Harry Potter", BigDecimal.valueOf(12.49), false, true);
         cashRegister.addProductToTransaction("Imported Box of Chocolates", BigDecimal.valueOf(10.00), true, true);
-//        cashRegister.addProductToTransaction("Imported Bottle of Perfume", BigDecimal.valueOf(47.50), true, false);
+        cashRegister.addProductToTransaction("Imported Bottle of Perfume", BigDecimal.valueOf(47.50), true, false);
 //        cashRegister.addProductToTransaction("Music CD", BigDecimal.valueOf(14.99), false, false);
+        calculator.addTaxes();
         BigDecimal actual = cashRegister.getTransactions().get(0).getPrice();
         BigDecimal expected = BigDecimal.valueOf(12.49).multiply(BigDecimal.valueOf(1.00)).setScale(2,
                 RoundingMode.HALF_EVEN);
@@ -60,10 +61,10 @@ public class TransactionCalculatorTest {
         BigDecimal expected2 = BigDecimal.valueOf(10.00).multiply(BigDecimal.valueOf(1.05)).setScale(2,
                 RoundingMode.HALF_EVEN);
         assertEquals(expected2, actual2);
-//        BigDecimal actual3 = cashRegister.getTransactions().get(0).getPrice();
-//        BigDecimal expected3 = BigDecimal.valueOf(47.50).multiply(BigDecimal.valueOf(1.15)).setScale(2,
-//                RoundingMode.HALF_EVEN);
-//        assertEquals(expected3, actual3);
+        BigDecimal actual3 = cashRegister.getTransactions().get(2).getPrice();
+        BigDecimal expected3 = BigDecimal.valueOf(47.50).multiply(BigDecimal.valueOf(1.15)).setScale(2,
+                RoundingMode.HALF_EVEN);
+        assertEquals(expected3, actual3);
 //        BigDecimal actual4 = cashRegister.getTransactions().get(0).getPrice();
 //        BigDecimal expected4 = BigDecimal.valueOf(14.99).multiply(BigDecimal.valueOf(1.10)).setScale(2,
 //                RoundingMode.HALF_EVEN);
--- src/tdd/TransactionCalculator.java
+++ src/tdd/TransactionCalculator.java
@@ -15,9 +15,24 @@ public class TransactionCalculator {
         BigDecimal temp = null;
         BigDecimal totalTaxes = BigDecimal.valueOf(0);
         for(Product product:cashRegister.getTransactions()){
-            temp = product.getPrice().multiply(BigDecimal.valueOf(1.10)).setScale(2, RoundingMode.HALF_EVEN);
-            totalTaxes = totalTaxes.add(product.getPrice().multiply(BigDecimal.valueOf(0.1))).setScale(2, RoundingMode.HALF_EVEN);
-            product.setPrice(temp);
+            if(product.isImported() && product.isTaxExempt()){
+                temp = product.getPrice().multiply(Constants.TAX_RATE_IMPORT).setScale(2, RoundingMode.HALF_EVEN);
+                totalTaxes =
+                        totalTaxes.add(product.getPrice().multiply(Constants.TAX_RATE_IMPORT.subtract(BigDecimal.valueOf(1)))).setScale(2,
+                        RoundingMode.HALF_EVEN);
+                product.setPrice(temp);
+            }
+            if(product.isImported() && !product.isTaxExempt()){
+                temp =
+                        product.getPrice().multiply(Constants.TAX_RATE_IMPORT.add(Constants.TAX_RATE_LOCAL.subtract(BigDecimal.valueOf(1)))).setScale(2,
+                        RoundingMode.HALF_EVEN);
+                totalTaxes =
+                        totalTaxes.add(product.getPrice().multiply(Constants.TAX_RATE_IMPORT.add(Constants.TAX_RATE_LOCAL).subtract(BigDecimal.valueOf(2)))).setScale(2,
+                                RoundingMode.HALF_EVEN);
+                product.setPrice(temp);
+            }
+
+
         }
         return totalTaxes;
     }
